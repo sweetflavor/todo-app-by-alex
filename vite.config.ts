cc47aa668b5d78ddf2c15d2775cd31ea9428ad81
Alex Man <sweet.devtools@gmail.com>
2022-09-23 12:46:02 +0200

    Base architecture

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,53 @@
+/// <reference types="vitest" />
+import { join } from 'path'
+import Components from 'unplugin-vue-components/vite'
+import AutoImport from 'unplugin-auto-import/vite'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  resolve: {
+    alias: [
+      {
+        find: /~(.+)/,
+        replacement: join(__dirname, '/node_modules/$1')
+      },
+
+      {
+        find: /@\//,
+        replacement: join(__dirname, 'src/')
+      },
+    ]
+  },
+
+  test: {
+    globals: true,
+    environment: 'happy-dom'
+  },
+
+  server: {
+    port: 4000,
+  },
+
+  plugins: [
+    vue(),
+    Components({
+      dts: true,
+      extensions: ['vue'],
+      dirs: [
+        'src/components',
+        'src/pages'
+      ]
+    }),
+    AutoImport({
+      imports: [
+        '@vueuse/head',
+        '@vueuse/core',
+        'vue',
+        'vue-router',
+        'vitest'
+      ],
+      dts: 'src/auto-import.d.ts'
+    })
+  ]
 })
